39f907fabe8559e0e6312d19f9713a78c6765450
RumenWisdom <Rumenwisdom@gmail.com>
2022-12-09 08:54:27 -0800

    cor

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,13 +3,12 @@
 
 /**
  * main - prints "with proper grammar, but the outcoome 
- * is a piece of art,"followed newline
+ * is a piece of art," followed newline
  * Return: Always 0.
  *
  */
 
 int main(void)
-
 
 {
 		printf("with proper grammar, but the outcome is a piece of art,\n");
