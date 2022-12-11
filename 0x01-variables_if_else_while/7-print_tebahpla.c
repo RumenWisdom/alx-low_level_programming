b25c3e4ac1bf9f579740b78e8a87143105106b6c
RumenWisdom <Rumenwisdom@gmail.com>
2022-12-11 08:08:27 -0800

    tebh

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <time.h>
 
 /**
@@ -13,6 +15,7 @@ int main(void)
 {
 
 	char low;
+
 	for (low = 'z'; low >= 'a'; low--)
 	putchar(low);
 	putchar('\n');
