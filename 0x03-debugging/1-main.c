34f59f6ba7b785e416681d7df5a75936d20477c6
RumenWisdom <Rumenwisdom@gmail.com>
2022-12-15 12:33:42 -0800

    desperate

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,7 +5,6 @@
 * Return: 0
 */
 
-
 int main(void)
 {
 	int i;
@@ -14,7 +13,7 @@ int main(void)
 	i = 0;
 	/*while (i < 10)*/
 	/*{*/
-	/*putchar(i);*/
+        /*        putchar(i);*/
 	/*}*/
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
