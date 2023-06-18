00662a9818f0127337aab632a9b84a9067f0627f
RumenWisdom <Rumenwisdom@gmail.com>
2023-06-18 13:16:45 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 48;
+	int bum = 97;
+
+	while (num <= 57)
+	{
+		putchar(num);
+		num++;
+	}
+
+	while (bum <= 102)
+	{
+		putchar(bum);
+		bum++;
+	}
+	return (0);
+}
