b723aa5db6e3940dbdadc3e408220d64aeb1b3ac
RumenWisdom <Rumenwisdom@gmail.com>
2023-06-21 03:50:47 -0700

    1-alphabet.c corrections

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,11 +11,9 @@ int main(void)
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
-	_putchar('t');
 	_putchar('c');
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
 	return (0);
-}
