7bc117449b2b1b505c1d4aaaf3e2981c343a1c7d
RumenWisdom <Rumenwisdom@gmail.com>
2023-06-21 13:38:38 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ *
+ * @i: number input
+ *
+ * Return: Absolute value
+ */
+int _abs(int);
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
