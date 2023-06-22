816425a597f84d865e8f3872a93d530f71513e79
RumenWisdom <Rumenwisdom@gmail.com>
2023-06-22 10:42:30 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isdigit - Entry point
+ *
+ * @c: integeral value from zero to nine
+ *
+ * Return: Always 0 (Success)
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	
+	}
+	putchar('\n');
+}
