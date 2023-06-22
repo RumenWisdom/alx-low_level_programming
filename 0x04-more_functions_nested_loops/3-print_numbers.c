e014a6555e89013f4020ace002e3bb184af719ef
RumenWisdom <Rumenwisdom@gmail.com>
2023-06-22 11:35:25 -0700

    ./3-print_numbers | cat -e

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+	{
+		_putchar(num);
+	}
+	_putchar('\n');
+}
