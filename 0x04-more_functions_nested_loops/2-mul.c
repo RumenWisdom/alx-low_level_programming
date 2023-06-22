c412fb71a7e705d231a9df2604e04b2fbba11044
RumenWisdom <Rumenwisdom@gmail.com>
2023-06-22 11:08:57 -0700

    2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - Entry point to multiplication
+ *
+ * @a: first integral input
+ *
+ * @b: second integral input
+ *
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+	int ab = a * b;
+	return (ab);
+}
