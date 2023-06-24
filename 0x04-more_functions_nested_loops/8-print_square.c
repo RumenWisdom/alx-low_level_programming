0968f90a3c5f69d2d74f9855480f2aea21c39871
RumenWisdom <Rumenwisdom@gmail.com>
2023-06-23 18:37:05 -0700

    much more corrections

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,20 +9,21 @@
  */
 void print_square(int size)
 {
- if (size <= 0)
- {
- _putchar('\n');
- } else
- {
- int a, b;
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int a, b;
 
- for (a = 0; a < size; a++)
- {
- for (b = 0; b < size; b++)
- {
- _putchar('#');
- }
- _putchar('\n');
- }
- }
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+			{
+				_putchar('#');
+			}
+		_putchar('\n');
+		}
+	}
 }
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,28 +8,32 @@
  */
 int main(void)
 {
- int a;
+	int a;
 
- for (a = 1; a <= 100; a++)
- {
- if (a % 3 == 0 && a % 5 != 0)
- {
- printf(" Fizz");
- } else if (a % 5 == 0 && a % 3 != 0)
- {
- printf(" Buzz");
- } else if (a % 3 == 0 && a % 5 == 0)
- {
- printf(" FizzBuzz");
- } else if (a == 1)
- {
- printf("%d", a);
- } else
- {
- printf(" %d", a);
- }
- }
- printf("\n");
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 3 == 0 && a % 5 != 0)
+		{
+			printf("Fizz");
+		}
+		else if (a % 5 == 0 && a % 3 != 0)
+		{
+			printf("Buzz");
+		}
+		else if (a % 3 == 0 && a % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (a == 1)
+		{
+			printf("%d", a);
+		}
+		else
+		{
+			printf(" %d", a);
+		}
+	}
+	printf("\n");
 
- return (0);
+	return (0);
 }
