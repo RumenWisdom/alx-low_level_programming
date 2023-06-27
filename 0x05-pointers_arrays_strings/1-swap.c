b440f207aff4fd44e1730a278f315868daf9ade0
RumenWisdom <Rumenwisdom@gmail.com>
2023-06-26 20:57:31 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * swap_int(int *a, int *b) - Entry point
+ *
+ * @a: integer input to be swapped
+ *
+ * @b: Second integer input to be swapped
+ *
+ * Return: Always 0 (Success)
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
